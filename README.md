37fc9b7685987a9ac5a79ffa16af37d4f6f95c2b
saurabhjn76 <saurabhjn76@gmail.com>
2017-10-16 12:39:28 +0530

    retractor code as per PEP8 guidelines and added output

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # LogAnalysis
 Project 3 of Udacity FSND
+
+Here are the questions the reporting tool should answer. The example answers given aren't the right ones, though!
+
+#### What are the most popular three articles of all time? Which articles have been accessed the most? Present this information as a sorted list with the most popular article at the top.
+
+Example:
+
+"Princess Shellfish Marries Prince Handsome" — 1201 views
+"Baltimore Ravens Defeat Rhode Island Shoggoths" — 915 views
+"Political Scandal Ends In Political Scandal" — 553 views
+
+
+####  Who are the most popular article authors of all time? That is, when you sum up all of the articles each author has written, which authors get the most page views? Present this as a sorted list with the most popular author at the top.
+
+Example:
+
+Ursula La Multa — 2304 views
+Rudolf von Treppenwitz — 1985 views
+Markoff Chaney — 1723 views
+Anonymous Contributor — 1023 views
+
+
+#### On which days did more than 1% of requests lead to errors? The log table includes a column status that indicates the HTTP status code that the news site sent to the user's browser.
+
+
+#### To run  the news database to its original form, first  drop each of the tables, then import the data from the newsdata.sql file.
+
+In psql:
+
+```
+drop table log;
+drop table articles;
+drop table authors;
+```
+Then in the shell, to import the data:
+
+```psql -d news -f newsdata.sql```
+
+Run the script:
+
+``` python main.py```
+
+The output is shown in  `out.txt`.
+
--- main.py
+++ main.py
@@ -1,34 +1,60 @@
 import psycopg2
 
-DBNAME="news"
+DBNAME = "news"
 
 
 '''
 Ques 1: What are the most popular three articles of all time?
-Ques 2: Who are the most popular article authors of all time? 
+Ques 2: Who are the most popular article authors of all time?
 Ques 3: On which days did more than 1% of requests lead to errors?
 '''
 
-query_1="select articles.title, count(*) as views from articles inner join log on log.path = concat('/article/', articles.slug) group by log.path, articles.title order by views desc limit 3;"
+query_1 = '''select articles.title, count(*) as views from articles
+ inner join log on log.path = concat('/article/', articles.slug)
+ group by log.path, articles.title order by views desc limit 3;'''
 
-query_2="select authors.name, count(*) as views from articles inner join authors on authors.id=articles.author inner join log on log.path = concat('/article/', articles.slug) group by authors.name order by views desc;"
-
-
-query_3=" select * from (select date, CAST((CAST(errorRequest AS float) * 100.0 ) / CAST(totalRequest AS float) as DECIMAL(16,2)) as errorpercentage from (select T.date, totalRequest, errorRequest  from (select date(time) as date, count(*) as errorRequest from log  where log.status like '%404%' group by date) as T inner join (select date(time) as date, count(*) as totalRequest  from log group by date) as E on T.date=E.date) as logtable)as errorRequest where errorpercentage >1.0;"
-
-
-
-
+query_2 = '''select authors.name, count(*) as views from articles
+inner join authors on authors.id=articles.author inner join
+log on log.path = concat('/article/', articles.slug)
+group by authors.name order by views desc;'''
 
 
+query_3 = '''select * from (select date,
+CAST((CAST(errorRequest AS float) * 100.0 ) /
+CAST(totalRequest AS float) as DECIMAL(16,2))
+ as errorpercentage from (select T.date, totalRequest, errorRequest
+  from (select date(time) as date, count(*) as errorRequest from log
+   where log.status like '%404%' group by date) as T inner join
+    (select date(time) as date, count(*) as totalRequest  from
+    log group by date) as E on T.date=E.date) as logtable) as
+    errorRequest where errorpercentage >1.0;'''
 
-
+ques1 = 'Ques 1: What are the most popular three articles of all time?'
+ques2 = 'Ques 2: Who are the most popular article authors of all time?'
+ques3 = 'Ques 3: On which days did more than 1% of requests lead to errors?'
 
 
 if __name__ == '__main__':
-	conn = psycopg2.connect(database =DBNAME)
-	curr = conn.cursor()
+    conn = psycopg2.connect(database=DBNAME)
+    curr = conn.cursor()
+    curr.execute(query_1)
+    res = curr.fetchall()
+    print ques1
+    for i in range(len(res)):
+        print i+1, ')', res[i][0], '--', res[i][1]
+    print ''
 
-	conn.close()
+    curr.execute(query_2)
+    res = curr.fetchall()
+    print ques2
+    for i in range(len(res)):
+        print i+1, ')', res[i][0], '--', res[i][1]
+    print ''
+    curr.execute(query_3)
+    res = curr.fetchall()
+    print ques3
+    for i in range(len(res)):
+        print i+1, ')', res[i][0], '--', res[i][1]
+    print ''
 
-
+    conn.close()
